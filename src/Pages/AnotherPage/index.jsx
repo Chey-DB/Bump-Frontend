7a03b56e83e137ff8fc4b78d7e1cc77797e4cd25
Geraldine <giquigeraldine@outlook.com>
2023-06-12 14:26:02 +0100

    starting template for frontend

---
+++ src/Pages/AnotherPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnotherPage = () => {
+  return (
+    <>
+      AnotherPage
+    </>
+  )
+}
+
+export default AnotherPage
---
+++ src/Pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from './Home';
+export { default as AnotherPage } from './AnotherPage';
+export { default as NotFound } from './NotFound';
