0839347bcad9523524b9e71a35d01f4d82c8d269
George Hill <georgeophill@outlook.com>
2023-06-14 16:09:13 +0100

    created motivationalQuotes file in components

---
+++ src/components/MotivationalQuotes/index.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState} from 'react'
+
+const MotivationalQuotes = () => {
+    const [quotes, setQuotes] = useState('')
+
+
+  return (
+    <div>MotivationalQuotes</div>
+  )
+}
+
+export default MotivationalQuotes
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as Header } from './Header'
 export { default as LoggedNav } from './LoggedNav'
 export { default as Calendar } from './Calendar'
 export { default as Checklist } from './Checklist'
+
