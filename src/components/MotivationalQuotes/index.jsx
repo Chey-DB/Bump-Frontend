c71604d26dfd2464b0c92c78ef27b1b70e99a146
George Hill <georgeophill@outlook.com>
2023-06-14 16:10:31 +0100

    useEffect for quotes api

--- src/components/MotivationalQuotes/index.jsx
+++ src/components/MotivationalQuotes/index.jsx
@@ -3,6 +3,14 @@ import React, { useEffect, useState} from 'react'
 const MotivationalQuotes = () => {
     const [quotes, setQuotes] = useState('')
 
+    useEffect(() => {
+        fetch('https://type.fit/api/quotes')
+        .then(response => response.json())
+        .then(data => {
+            const randomQuote = Math.floor(Math.random() * data.length)
+            setQuotes(data[randomQuote].text)
+        })      
+    })
 
   return (
     <div>MotivationalQuotes</div>
