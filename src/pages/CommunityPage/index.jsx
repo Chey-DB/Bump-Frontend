bce55d3560e2a82dae7781cc18818985ef5bf2c4
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-14 17:12:13 +0100

    add .env to git ignore, import cloudianry package

--- src/pages/CommunityPage/index.jsx
+++ src/pages/CommunityPage/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import {
+  Image,
+  Video,
+  Transformation,
+  CloudinaryContext,
+} from "cloudinary-react";
+import "./styles.css";
 
 const CommunityPage = () => {
   return (
     <>
-    <div className='container'>
-      <h1>Community Page</h1>
+      <div className="container">
+        {/* <h1>Community Page</h1>
       <h2>This is something</h2>
       <br />
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iste, fugiat neque quod ipsa error alias totam dolorum sapiente explicabo?</h3>
@@ -13,10 +19,17 @@ const CommunityPage = () => {
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio, quaerat cumque et id sapiente accusantium commodi earum cupiditate exercitationem impedit veritatis natus pariatur repellendus odit nostrum at aut aspernatur. Perferendis!</p>
       <div className='button-div'>
         <button>Press Me</button>
+      </div> */}
+        <CloudinaryContext cloudName="dzbvvdev4">
+          <div>
+            <Image publicId="sample" width="50" />
+          </div>
+          <Image publicId="sample" width="0.5" />
+        </CloudinaryContext>
+        {}
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default CommunityPage
+export default CommunityPage;
