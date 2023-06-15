8fb152241072998c41a8062fd2ca96aa9b97d3ef
George Hill <georgeophill@outlook.com>
2023-06-15 17:06:41 +0100

    progress bar weeks below added

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import './styles.css'
+import React from 'react';
+import './styles.css';
 
 const ProgressBar = ({ dueDate, currentWeek }) => {
+  const calculateProgress = () => {
+    const totalWeeks = Math.ceil((dueDate - new Date()) / (1000 * 60 * 60 * 24 * 7));
+    const progressPercentage = (currentWeek / totalWeeks) * 100;
+    return Math.min(progressPercentage, 100);
+  };
 
-    const calculateProgress = () => {
-        const totalWeeks = Math.ceil((dueDate - new Date()) / (1000 * 60 * 60 * 24 * 7));
-        const progressPercentage = ((currentWeek / totalWeeks) * 100)
-        return Math.min(progressPercentage, 100)
-    }
-
-
+  const renderWeeksText = () => {
+    const totalWeeks = Math.ceil((dueDate - new Date()) / (1000 * 60 * 60 * 24 * 7));
+    const progressWeeks = Math.floor((currentWeek / totalWeeks) * totalWeeks);
+    return `Week: ${progressWeeks}`;
+  };
 
   return (
     <div className="progress-bar-container">
-      <h3>Progress Bar </h3>
+      <h3>Progress Bar</h3>
       <div className="progress-bar">
         <div className="progress-bar-fill" style={{ width: `${calculateProgress()}%` }}></div>
       </div>
+      <p>{renderWeeksText()}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
