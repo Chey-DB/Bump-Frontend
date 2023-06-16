33517f76a397a47d3c1413a126afee91aa8bba13
Chey-DB <cheydb@rocketmail.com>
2023-06-16 15:05:58 +0100

    Fixed AuthProvider bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./Context";
+import ProtectedRoute from "./Route";
 
 import * as Pages from "./pages";
 // import Header from './components/Header'
@@ -11,16 +12,18 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<LoggedNav />}>
-          <Route index element={<Pages.HomePage />} />
-          <Route path="/register" element={<Pages.RegisterPage />} />
-          <Route path="/login" element={<Pages.LoginPage />} />
-          <Route path="/loading" element={<Pages.LoadingPage />} />
-          <Route path="/dashboard" element={<Pages.DashboardPage />} />
-          <Route path="/calendar" element={<Pages.CalendarPage />} />
-          <Route path="/my-journal" element={<Pages.MyJournalPage />} />
-          <Route path="/community" element={<Pages.CommunityPage />} />
-          <Route path="/faqs" element={<Pages.FAQsPage />} />
+        <Route index element={<Pages.HomePage />} />
+        <Route path="/register" element={<Pages.RegisterPage />} />
+        <Route path="/login" element={<Pages.LoginPage />} />
+        <Route path="/loading" element={<Pages.LoadingPage />} />
+        <Route path="/" element={<ProtectedRoute redirectTo="/" />}>
+          <Route path="/" element={<LoggedNav />}>
+            <Route path="/dashboard" element={<Pages.DashboardPage />} />
+            <Route path="/calendar" element={<Pages.CalendarPage />} />
+            <Route path="/my-journal" element={<Pages.MyJournalPage />} />
+            <Route path="/community" element={<Pages.CommunityPage />} />
+            <Route path="/faqs" element={<Pages.FAQsPage />} />
+          </Route>
           <Route path="*" element={<Pages.NotFoundPage />} />
         </Route>
       </Routes>
--- src/pages/LoadingPage/index.jsx
+++ src/pages/LoadingPage/index.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import axios from 'axios'
 import { useAuth } from '../../Context'
+import { useEffect } from 'react'
 
 
 const LoadingPage = () => {
     const { user, setUser } = useAuth();
 
-    axios.get('http://localhost:3000/auth/checkUser', { withCredentials: true })
-    .then(response => {
-      const userId = response.data._id
-      const username = response.data.google.username
-      if (response.data) {
-        // User is logged in
-        // Use the user data
-        console.log(response.data._id);
-      } else {
-        // User is not logged in
-        console.log("User is not logged in");
-      }
-    });
+    useEffect(() => {
+        axios.get('http://localhost:3000/auth/checkUser', { withCredentials: true })
+            .then(response => {
+                const userId = response.data._id
+                const username = response.data.google.username
+                if (response.data) {
+                    setUser({ userId, username })
+                    // User is logged in
+                    // Use the user data
+                    console.log(response.data._id);
+                } else {
+                    // User is not logged in
+                    console.log("User is not logged in");
+                }
+            });
+    }, [])
 
   return (
     <div>LoadingPage</div>
