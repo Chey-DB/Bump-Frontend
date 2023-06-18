c4b8df6683cdc647e2d9f6e2477fde69051a336d
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-06-18 21:27:50 +0100

    style changes and checklist fixes

--- src/components/MotivationalQuote/index.jsx
+++ src/components/MotivationalQuote/index.jsx
@@ -36,10 +36,8 @@ const MotivationalQuote = () => {
       <div className="motivational-quote-container">
         <img src="quotation-marks.png" alt="quotation-marks" className='quotation-mark-img' id='left-qm' />
         <div className="motivational-quote">
-          <span className="quote-mark quote-mark-left">&#8220;</span>
-          <h3>Motivational Quote of the day:</h3>
-          <p>{quote}</p>
-          <span className="quote-mark quote-mark-right">&#8220;</span>
+          <h3 id='quote-header'>Motivational Quote of the day:</h3>
+          <h2 id='quote-content'>{quote}</h2>
         </div>
         <img src="quotation-marks.png" alt="quotation-marks" className='quotation-mark-img' id='right-qm' />
       </div>
