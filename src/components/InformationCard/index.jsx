fe6b24f586a0ae423b43ce961f4c57f47cc4186c
Chey-DB <cheydb@rocketmail.com>
2023-06-20 10:17:55 +0100

    implemented functionality for registering using the local method and linked information card to NHS API

--- src/components/InformationCard/index.jsx
+++ src/components/InformationCard/index.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 import './styles.css';
 import babyGirl from './baby-girl.png';
 import woman from './woman.png'
+import GlobalModal from '../GlobalModal';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const InformationCard = () => {
+  const [show, setShow] = useState(false);
+  const [response, setResponse] = useState("");
+  const [week, setWeek] = useState("1-2-3-weeks");
+  const [trim, setTrim] = useState("1-to-12");
+  useEffect(() => {
+    const getInfo = async () => {
+      
+ 
+      const info = await axios.get(
+        `https://api.nhs.uk/pregnancy/week-by-week/${trim}/${week}/`,
+        { headers: { "subscription-key": "9286783e6cba427d83f51dfb70be451f" } }
+        );
+        const response = info.data.mainEntityOfPage[0].hasPart[0].text;
+      console.log(response);
+      setResponse(response);
+    };
+    getInfo();
+  }, []);
+  
   return (
     <div className="cards-container">
       <div className="information-card-container">
@@ -16,7 +38,9 @@ const InformationCard = () => {
             <div className="image-container">
               <img src={babyGirl} alt="Card 1" className="information-card-image" />
             </div>
-            <a href="#" className="button-link">Read More</a>
+            <button className="button-link" onClick={() => setShow(true)}>Read More</button>
+            <GlobalModal show={show} onClose={() => setShow(false)} children={<div dangerouslySetInnerHTML={{ __html: response }}>
+        </div>}></GlobalModal>
           </div>
         </div>
         <div className="information-card">
@@ -28,7 +52,8 @@ const InformationCard = () => {
             <div className="image-container">
               <img src={woman} alt="Card 2" className="information-card-image" />
             </div>
-            <a href="#" className="button-link">Read More</a>
+            <button className="button-link" onClick={() => setShow(true)}>Read More</button>
+            {/* <GlobalModal show={show} onClose={() => setShow(false)}></GlobalModal> */}
           </div>
         </div>
         <div className="information-card">
@@ -40,7 +65,8 @@ const InformationCard = () => {
             <div className="image-container">
               <img src={babyGirl} alt="Card 3" className="information-card-image" />
             </div>
-            <a href="#" className="button-link">Read More</a>
+            <button className="button-link" onClick={() => setShow(true)}>Read More</button>
+            {/* <GlobalModal show={show} onClose={() => setShow(false)}></GlobalModal> */}
           </div>
         </div>
       </div>
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import './styles.css'
+import { useAuth } from '../../Context/'
 
 const RegisterPage = () => {
+  const navigate = useNavigate()
+  const { user, setUser } = useAuth()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleRegister = (e) => {
+    console.log('Registering')
+    e.preventDefault()
+    axios.post('http://localhost:3000/local-users/register', {      
+      username,
+      password     
+    }, { withCredentials: true })
+      .then(response => {
+        setUser({ username: response.data.username, id: response.data.id })
+        console.log(response)
+        // if (response.data) {
+          navigate('/dashboard')
+        // } else {
+        //   console.log('Register failed')
+      //   }
+      })
+      .catch(error => {
+        console.log(error)
+      }
+    )
+  }
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+
+
   const handleGoogleSignIn = () => {
     window.open('http://localhost:3000/auth/google', '_self')
-
   }
     
   console.log(document.cookie)
@@ -13,12 +53,12 @@ const RegisterPage = () => {
       <button onClick={handleGoogleSignIn}>Sign in with Google</button>
       <form action="POST">
         <label htmlFor="username">Username:</label>
-        <input type="text" name="username" id="username" placeholder="Username" />
+        <input onChange={handleUsernameChange}type="text" name="username" id="username" placeholder="Username" />
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" placeholder="Password" />
-        <label htmlFor="confirmPassword">Password (confirm):</label>
-        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" />
-        <button type="submit">Register</button>
+        <input onChange={handlePasswordChange}type="password" name="password" id="password" placeholder="Password" />
+        {/* <label htmlFor="confirmPassword">Password (confirm):</label>
+        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" /> */}
+        <button type="submit" onClick={handleRegister}>Register</button>
       </form>
     </div>
   )
