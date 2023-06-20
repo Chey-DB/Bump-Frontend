31a431fef2202408176d646acfa2a590c2b81620
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-06-20 23:37:28 +0100

    add comments for understanding code

--- src/components/GlobalModal/index.jsx
+++ src/components/GlobalModal/index.jsx
@@ -12,18 +12,18 @@ const GlobalModal = props => {
 
   const calculateModalPosition = () => {
     const viewportWidth = window.innerWidth;
-    const viewportHeight = window.innerHeight;
+    const viewportHeight = window.innerHeight; //get height and width of viewport on any screen
 
     const modalWidth = modalRef.current.offsetWidth;
-    const modalHeight = modalRef.current.offsetHeight;
+    const modalHeight = modalRef.current.offsetHeight; //get the hight and width of modal
 
-    const scrollY = window.scrollY || window.pageYOffset; // Account for scroll position
+    const scrollY = window.scrollY; //get position of scroll (how many pixels have been scrolled)
 
     const top = scrollY + (viewportHeight - modalHeight) / 2;
-    const left = (viewportWidth - modalWidth) / 2;
+    const left = (viewportWidth - modalWidth) / 2; //values for setting top and left 
 
     modalRef.current.style.top = `${top}px`;
-    modalRef.current.style.left = `${left}px`;
+    modalRef.current.style.left = `${left}px`; //set top and left positions
   };
 
   if (!props.show) {
