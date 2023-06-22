4f90bd9b5bcd5adeed5511d0e91976ea06b2c538
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-06-22 23:39:47 +0100

    styling start on home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -40,7 +40,17 @@ const HomePage = () => {
         </div>
 
         <div className="hp-mid-section">
-
+          <div className='hp-mid-wrapper'>
+            <p>Birth is something that unites all of mankind. It's such a beautiful and magical thing.
+              Bump was created so that the process that leads up to birth (pregnancy) can truly be a time of joy, peace and magic.
+              Pregnancy leads to the creation of life but no two pregnancies are the same. Everyone perceives it differently.
+              However, many women look back on their pregnancy and don't think of it as a nice experience due to stress, anxiety and worries related to not knowing what they need to know and even as far as feeling ill-equipped to be a mother. This can often continue postpartum!
+              Bump is an app created with one goal in mind. That is, ensuring that pregnant women all around the world are able to have a pleasant experience during pregnancy by receiving the support they need, when they need it, according to various needs that a pregnant woman might have.
+              The way in which Bump achieves this is by....</p>
+            <div className='hp-image-wrapper'>
+              {/* <img src="communitiespg.png" alt="" /> */}
+            </div>
+          </div>
         </div>
         <div className='hp-end-section'>
 
