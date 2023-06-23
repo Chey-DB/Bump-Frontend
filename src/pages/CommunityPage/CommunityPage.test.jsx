b7165d7eee17e73194325775a5c39f0ca65e940c
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-23 02:18:41 +0100

    61% test coverage, but all tests failed

--- src/pages/CommunityPage/CommunityPage.test.jsx
+++ src/pages/CommunityPage/CommunityPage.test.jsx
@@ -8,6 +8,22 @@ import matchers from "@testing-library/jest-dom/matchers";
 expect.extend(matchers);
 
 import CommunityPage from ".";
+import { PostCard } from "../../components";
+
+const mockData = [
+  {
+    key: "123asd",
+    id: "123asd",
+    user_id: "asd123",
+    title: "testing title",
+    content: "testing content",
+    image: "https://www.google.com/",
+    comments: [],
+    question: false,
+    createdAt: "23/-6/2023",
+    updatedAt: "23/-6/2023",
+  },
+];
 
 describe("Post Card component", () => {
   beforeEach(() => {
@@ -19,5 +35,17 @@ describe("Post Card component", () => {
   it("Display card div", () => {
     const elem = screen.getByRole("div");
     expect(elem).toBeInTheDocument();
+  });
+
+  it("test getPosts", () => {
+    const mockFetchData = jest
+      .spyOn(CommunityPage, "getPosts")
+      .mockImplementation(async () => {
+        return [
+          {
+            name: "kunal",
+          },
+        ];
+      });
   });
 });
