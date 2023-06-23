f256b3a54faf68b2daea94ecaa637c64bc258810
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-23 01:22:39 +0100

    add test files for each components

--- src/components/Calendar/Calendar.test.jsx
+++ src/components/Calendar/Calendar.test.jsx
@@ -1,27 +1,28 @@
-import React from 'react'; 
-import { describe, it, expect, beforeEach, afterEach } from 'vitest'; 
-import { screen, render, cleanup } from '@testing-library/react'; 
+import React from "react";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { screen, render, cleanup } from "@testing-library/react";
 
-import matchers from '@testing-library/jest-dom/matchers';
-expect.extend(matchers); 
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
 
-import LoggedNav from '.';
-import { MemoryRouter } from 'react-router-dom';
+import LoggedNav from ".";
+import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
-describe('LoggedNav', () => {
-
+describe("LoggedNav", () => {
   beforeEach(() => {
     render(
       <MemoryRouter>
         <LoggedNav />
       </MemoryRouter>
-    )
+    );
   });
 
   afterEach(() => {
     cleanup();
   });
-
-})
-
+  it("Display card div", () => {
+    const elem = screen.getByRole("div");
+    expect(elem).toBeInTheDocument();
+  });
+});
